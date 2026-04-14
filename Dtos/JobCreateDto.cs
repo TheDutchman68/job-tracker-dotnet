b967634f6beb0d39c9babdc20b2c4b107b4f0ba1
TheDutchman68 <nd527589@gmail.com>
2026-04-14 17:10:43 +0300

    feat: add validation rules to JobCreateDto including enum constrains

--- Dtos/JobCreateDto.cs
+++ Dtos/JobCreateDto.cs
@@ -1,10 +1,23 @@
 using JobTracker.Api.Models;
+using System.ComponentModel.DataAnnotations;
 namespace JobTracker.Api.Dtos;
 
 public class JobCreateDto
 {
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
     public string Company { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
     public string Position { get; set; }
+
+    [Required]
+    [EnumDataType(typeof(JobStatus))]
     public JobStatus Status { get; set; }
+    
+    [MaxLength(100)]
     public string Location {get; set;}
 }
